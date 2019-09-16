d77f9e8b1a225bd3cbfa96c0c6f520f300c194a3
huang16-z7u <1642387417@qq.com>
2019-09-16 21:30:37 +0800

    Update dataclean.py
    waste

--- dataclean.py
+++ dataclean.py
@@ -4,9 +4,21 @@ import re
 
 DATAPATH='./data/'
 DATASETS=[]
+DATASET_LABELS=[]
 datas=[]
 for dataset in DATASETS:
     data=pd.read_csv(DATAPATH+dataset)
-    for content in data['content']:
-        sentences=re.split('([！？。])',content)
-    datas.append(data)
+    for line in data.shape[0]:
+        sentence_id=data['id'][line]
+        sentence_list=[]
+        sentence_list.append(data['title'][line])
+        content_fragment=re.split('([！？。])',data['content'])
+        for i in range(0,len(content_fragment)-1,2):
+            sentence_list.append(content_fragment[i]+content_fragment[i+1])
+        # TODO clean jscode in the content
+        with open(DATAPATH+'OUTPUT_'+dataset,'w') as output:
+            text='id,sentence\n'
+            for sentence in sentence_list:
+                text=text+'%s,%s\n'%(sentence_id,sentence)
+                # TODO tran sentence to vec by using bert
+            output.write(text)
