1ec92a7057b3774a7970cfca47f55ef19f3eb329
huang16-m <1642387417@qq.com>
2019-09-17 13:24:01 +0800

    9.17noon

--- dataclean.py
+++ dataclean.py
@@ -8,22 +8,25 @@ DATAPATH='~/DataSets/df1/'
 BERTPATH=os.path.expanduser(BERTPATH)
 DATAPATH=os.path.expanduser(DATAPATH)
 OUTPUTPATH='./data/'
-DATASETS=[]
-DATASET_LABELS=[]
+DATASETS=['DEV_Train_DataSet.csv']
+DATASET_LABELS=['DEV_Train_DataSet_Label.csv']
 datas=[]
+answer=pd.read_csv(DATAPATH+DATASET_LABELS[0],index_col=0)
+ans_dict=answer.to_dict()['label']
 for dataset in DATASETS:
     data=pd.read_csv(DATAPATH+dataset)
     for line in data.shape[0]:
         sentence_id=data['id'][line]
         sentence_list=[]
         sentence_list.append(data['title'][line])
-        content_fragment=re.split('([！？。])',data['content'])
+        content_fragment=re.split('([！？。])',data['content'][line])
         for i in range(0,len(content_fragment)-1,2):
-            sentence_list.append(content_fragment[i]+content_fragment[i+1])
-        # TODO clean jscode in the content
-        with open(DATAPATH+'OUTPUT_'+dataset,'w') as output:
-            text='id,sentence\n'
+            sentence_sym=content_fragment[i]+content_fragment[i+1]
+            sentence_sym_c=re.sub('[\u0020-\u007e]{5,}','',sentence_sym)
+            sentence_list.append(sentence_sym_c)
+        with open(OUTPUTPATH+'OUTPUT_'+dataset,'w') as output:
+            text='id,sentence,label\n'
             for sentence in sentence_list:
-                text=text+'%s,%s\n'%(sentence_id,sentence)
+                text=text+'%s,%s,%d\n'%(sentence_id,sentence,ans_dict[sentence_id])
                 # TODO tran sentence to vec by using bert
             output.write(text)
